58f20715801ea74bb3589c927a47a61755dd59ed
Srivas Sundar <srivas.srihari@gmail.com>
2016-09-25 02:15:41 -0400

    getFlights

--- getFlightSchedule.py
+++ getFlightSchedule.py
@@ -3,52 +3,80 @@ from urllib2 import urlopen
 from urllib import urlencode
 
 
-def getFlightSchedule(event, context):
+def getFlights(event, context):
 
     depDate = event['request']['intent']['slots']['DepartureDate']['value']
-    depCode = event['request']['intent']['slots']['DepartureCode']['value']
-    arrCode = event['request']['intent']['slots']['ArrivalCode']['value']
-    todayDate = date.today().strftime("%Y-%m-%d")
+    retDate = \
+        event['request']['intent']['slots']['ReturnDate']['value'] or None
+    origin = event['request']['intent']['slots']['Origin']['value']
+    dest = event['request']['intent']['slots']['Destination']['value']
 
     data = {
         'apikey': 'zCQj3XGAs1q2tQ3RKGdk3XLJ4dQqp2NF',
-        'todayDate': todayDate,
+        'retDate': retDate,
         'depDate': depDate,
-        'depCode': depCode,
-        'arrCode': arrCode
+        'origin': origin,
+        'dest': dest
     }
 
-    url = 'https://demo30-test.apigee.net/v1/hack/schedule' + \
-        '?todayDate={todayDate}' + \
-        '&departureDate={depDate}' + \
-        '&departureAirportCode={depCode}' + \
-        '&arrivalAirportCode={arrCode}' + \
-        '&apikey={apikey}'.format(**data)
+    url = ('https://demo30-test.apigee.net/v1/hack/search/flight' +
+           '?origin={origin}' +
+           '&destination={dest}' +
+           '&departDate={depDate}' +
+           ('&returnDate={retDate}' if retDate else "") +
+           '&apikey={apikey}').format(**data)
 
     # print url
     r = urlopen(url)
-    result = r.json()['flightSchedulesResponse']
-    if result['status'] = 'FAIL':
+    result = json.load(r)
+    if 'itineraries' not in result:
         return construct_response(
             "Sorry, there was an error. Please try again!")
 
-    result = result['schedulesResponse']['flightScheduleTO']
-    arrCity = result['arrivalCityName']
-    depCity = result['departureCityName']
-    flights = result['flightScheduleSliceTOList']
+    itineraries = result['schedulesResponse']['itineraries'][:5]
 
-    out = "There are {numFlights} flights from {arrCity} to {depCity}" + \
-        " on {depDate}.".format(
-            numFlights=numFlights,
-            arrCity=arrCity,
+    out = ("There are flights available from {origin} to {dest}" +
+           " on {depDate}.").format(
+            origin=result['origin']['name'],
+            dest=result['destination']['name'],
             depCity=depCity
             )
 
+    for it in itineraries:
+        stops = it['slice']['stops']
+        stops = str(stops) if stops else "non"
+
+        fare = str(it['bestFare']['fareValue'])
+
+        flights = it['slice']['flights']
+
+        departTime = flight[0]['departTime']
+        arrivalTime = flight[-1]['arriveTime']
+        day = ("" if flight[0]['departDate'] == flight[-1]['arriveDate']
+               else (" on " + str(flight[-1]['arriveDate'])))
+
+        duration = it['slice']['duration']
+
+        message = ("For {fare} dollars, there is a {stops} stop flight" +
+                   "leaving at {depTime} local time and reaching at " +
+                   "{arrTime} local time{day}. Total travel time is " +
+                   "{duration}."
+                   ).format(
+                    fare=fare,
+                    stops=stops,
+                    depTime=departTime,
+                    arrTime=arrivalTime,
+                    day=day,
+                    duration=duration
+                   )
+
+        out += (" " + message)
+    out += " For more information please visit www.delta.com."
     return construct_response(out)
 
 
 def construct_response(output):
-    return return {
+    return {
         'version': '1.0',
         'sessionAttributes': {},
         'response': {
